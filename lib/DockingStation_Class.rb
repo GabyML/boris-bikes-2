c8ad0f5fc1b6f99337a2cb1e9cbc3d0f214c9e3c
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-6.local>
2015-07-22 12:51:50 +0100

    Finished challenge 10, docking station releases working bikes

--- lib/DockingStation_Class.rb
+++ lib/DockingStation_Class.rb
@@ -1,6 +1,9 @@
+require_relative 'Bike_Class'
+
 class DockingStation #definition of DockingStation class
-	
+
 	def release_bike #definition of release_bike method
+		Bike.new
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,13 @@
 require 'DockingStation_Class'
+require 'Bike_Class'
 
 describe DockingStation do
 	it {is_expected.to respond_to(:release_bike)}
+
+	it "releases a working bike" do
+		bike = subject.release_bike
+		expect(bike).to be_working
+	end
+
 	
 end
