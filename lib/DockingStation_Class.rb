447df3836ea37bdcb19ea67a5dcab7e90a7b56da
GabyML <gabriela.lagunes.11@uclac.uk>
2015-07-22 16:11:07 +0100

    Create guard condition for bikes' availability

--- lib/DockingStation_Class.rb
+++ lib/DockingStation_Class.rb
@@ -3,7 +3,13 @@ require_relative 'Bike_Class'
 class DockingStation #definition of DockingStation class
 
 	def release_bike #definition of release_bike method
-		Bike.new
+		fail 'No bikes available' unless @bike #Guard Condition (presence of bikes)
+		@bike # All Ruby instance variables are initially nil by default
 	end
+
+	def dock (bike)
+		@bike = bike
+	end
+
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,9 +5,17 @@ describe DockingStation do
 	it {is_expected.to respond_to(:release_bike)}
 
 	it "releases a working bike" do
+		subject.dock Bike.new #Added to change defauld nil
 		bike = subject.release_bike
 		expect(bike).to be_working
 	end
 
-	
+	it { is_expected.to respond_to(:dock).with(1).argument }
+
+	describe '#release_bike' do
+		it 'raises an error when there are no bikes available' do
+			expect { subject.release_bike }.to raise_error 'No bikes available'
+		end
+	end
+
 end
