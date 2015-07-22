15cee6e7e0fbbab23bf679195713f49296a307b6
GabyML <gabriela.lagunes.11@uclac.uk>
2015-07-22 12:04:39 +0100

    Creation of release_bike method, challenge 9

--- lib/DockingStation_Class.rb
+++ lib/DockingStation_Class.rb
@@ -1,2 +1,6 @@
-class DockingStation
+class DockingStation #definition of DockingStation class
+	
+	def release_bike #definition of release_bike method
+	end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
 require 'DockingStation_Class'
 
 describe DockingStation do
-	
+	it {is_expected.to respond_to(:release_bike)}
 	
 end
